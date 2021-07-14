bfcf7ccba9fbda6cdb32f94f7b7d4c310546b28d
Luis <nucleodeproducoes@hotmail.com>
2021-07-14 19:45:35 -0300

    Migration DB

--- ProjetoMVC/Startup.cs
+++ ProjetoMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace ProjetoMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ProjetoMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("ProjetoMVCContext"), builder =>
+                            builder.MigrationsAssembly("ProjetoMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

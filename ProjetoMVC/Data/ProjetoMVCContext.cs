c37845a1cf85cb574f6fa32e6ec2a6b21a9b784d
Luis <nucleodeproducoes@hotmail.com>
2021-07-14 18:45:39 -0300

    CRUD Scaffolding

---
+++ ProjetoMVC/Data/ProjetoMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Models;
+
+namespace ProjetoMVC.Data
+{
+    public class ProjetoMVCContext : DbContext
+    {
+        public ProjetoMVCContext (DbContextOptions<ProjetoMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoMVC.Models.Department> Department { get; set; }
+    }
+}
--- ProjetoMVC/Startup.cs
+++ ProjetoMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Data;
 
 namespace ProjetoMVC
 {
@@ -33,6 +35,9 @@ namespace ProjetoMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
